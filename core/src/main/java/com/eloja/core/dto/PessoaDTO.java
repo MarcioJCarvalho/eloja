7f1271f9c03011f8d7dbd4cfe0af1a389594cf41
pablinn <pablo.domiciano2019@gmail.com>
2023-05-28 19:53:16 -0300

    feature: Adicionado PessoaDTO

---
+++ core/src/main/java/com/eloja/core/dto/PessoaDTO.java
@@ -0,0 +1,13 @@
+package com.eloja.core.dto;
+
+import com.eloja.core.entity.Endereco;
+import com.eloja.core.entity.Telefone;
+
+import java.util.List;
+
+public class PessoaDTO {
+    private Endereco endereco;
+
+    private List<Telefone> telefones;
+
+}
