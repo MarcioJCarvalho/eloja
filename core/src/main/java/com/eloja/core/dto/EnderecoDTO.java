1d5f7b4bc5a36286182c86ebe8dda2b588fd96ef
pablinn <pablo.domiciano2019@gmail.com>
2023-05-28 20:09:23 -0300

    feature: Adicionado EnderecoDTO

---
+++ core/src/main/java/com/eloja/core/dto/EnderecoDTO.java
@@ -0,0 +1,31 @@
+package com.eloja.core.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class EnderecoDTO {
+    Integer id;
+
+    @NotEmpty
+    private String cep;
+
+    @NotEmpty
+    private String logradouro;
+
+    @NotNull
+    private String numero;
+
+    private String complemento;
+
+    @NotEmpty
+    private String bairro;
+
+    @NotEmpty
+    @Size(min = 7, max = 7, message = "O tamanho deve ser 7")
+    private String ibge;
+
+    @NotEmpty
+    @Size(min = 2, max = 2, message = "O tamanho deve ser 2")
+    private String uf;
+}
