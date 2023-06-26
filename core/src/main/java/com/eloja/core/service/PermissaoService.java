72e82149db864f4602a90bb4c988d0f212f7a3ec
Aluno IFPR <laboratorio@ifpr.edu>
2023-06-26 20:45:11 -0300

    feature: Criação do DTO e organização da PermissaoService.

--- core/src/main/java/com/eloja/core/service/PermissaoService.java
+++ core/src/main/java/com/eloja/core/service/PermissaoService.java
@@ -1,7 +1,9 @@
 package com.eloja.core.service;
 
+import com.eloja.core.dto.PermissaoDTO;
 import com.eloja.core.entity.Permissao;
 import com.eloja.core.repository.PermissaoRepository;
+import com.eloja.core.utils.ParseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +15,16 @@ public class PermissaoService {
     @Autowired
     private PermissaoRepository permissaoRepository;
 
-    public Permissao salvar(Permissao permissao){
-        return permissaoRepository.saveAndFlush(permissao);
+    public PermissaoDTO salvar(PermissaoDTO permissao){
+        Permissao entidadePermissao = ParseUtils.parse(permissao, Permissao.class);
+        PermissaoDTO permissaoDto = ParseUtils.parse(permissaoRepository.saveAndFlush(entidadePermissao), PermissaoDTO.class);
+        return permissaoDto;
     }
 
-    public Permissao editar(Permissao permissao){
-        return permissaoRepository.saveAndFlush(permissao);
+    public PermissaoDTO editar(PermissaoDTO permissao){
+        Permissao entidadePermissao = ParseUtils.parse(permissao, Permissao.class);
+        PermissaoDTO permissaoDto = ParseUtils.parse(permissaoRepository.saveAndFlush(entidadePermissao), PermissaoDTO.class);
+        return permissaoDto;
     }
 
     public void excluir(Integer id){
