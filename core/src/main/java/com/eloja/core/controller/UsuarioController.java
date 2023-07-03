9c9895ed5501216c160da421be6352155e3d20ba
diogo <diogo.meneses.franca@gmail.com>
2023-07-03 20:54:01 -0300

    Refactor:  - Concluída inversão de dependencia de UsuarioService;

--- core/src/main/java/com/eloja/core/controller/UsuarioController.java
+++ core/src/main/java/com/eloja/core/controller/UsuarioController.java
@@ -4,6 +4,9 @@ import com.eloja.core.dto.UsuarioDTO;
 import com.eloja.core.entity.Usuario;
 import com.eloja.core.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,12 +14,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-    @Autowired
     private UsuarioService usuarioService;
 
+    public UsuarioController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
     @PostMapping
-    public Usuario salvar(@RequestBody UsuarioDTO usuarioDto){
+    public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO usuarioDto){
         return usuarioService.salvar(usuarioDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> editar(@RequestBody UsuarioDTO usuarioDTO){
+        return usuarioService.editar(usuarioDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluir(@PathVariable("id") Integer usuarioId){
+        return usuarioService.excluir(usuarioId);
+    }
+
+    @GetMapping
+    public Page<UsuarioDTO> listarTodos(Pageable pageable){
+        return usuarioService.listarTodos(pageable);
     }
 
 
